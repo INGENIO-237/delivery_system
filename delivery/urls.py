b0a6762221834200def40ef4e8a5a4de06cc3e55
INGENIO <ingeniokhalif@gmail.com>
2022-11-08 17:29:56 +0100

    Simple commit

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth
 
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+
+    path('login/', auth.LoginView.as_view(template_name='sign_in.html')),
+    path('logout/', auth.LogoutView.as_view(next_page='/')),
+
     path('customer/', views.customer_page, name='customer'),
     path('courier/', views.courier_page, name='courier'),
 ]
